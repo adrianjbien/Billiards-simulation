5f9849d0e874bdade69b71bad89d7bd9a519708f
MrAdrianBn <139548121+MrAdrianBn@users.noreply.github.com>
2024-04-25 00:01:29 +0200

    Data layer in progress

---
+++ Dane/BilaPrototyp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public abstract class BilaPrototyp
+    {
+        protected double posX;
+        protected double posY;
+        protected double mass;
+        protected int size;
+        protected int vel;
+        protected int dir;
+
+        public BilaPrototyp Copy(int posX, int posY, double mass, int size, int vel, int dir)
+        {
+            this.posX = posX;
+            this.posY = posY;
+            this.vel = vel;
+            this.dir = dir;
+            this.mass = mass;
+            this.size = size;
+            return this;
+        }
+    }
+}
---
+++ Dane/StolPrototyp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public abstract class StolPrototyp
+    {
+        public int _width;
+        public int _height;
+
+        public StolPrototyp Copy(int width, int height)
+        {
+            this._width = width;
+            this._height = height;
+            return this;
+        }
+    }
+}
